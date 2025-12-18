ea987de4284ab91749d40b122f5f690064f6e359
daridev <darideveloper@gmail.com>
2025-12-17 21:29:58 -0600

    feat (Header): update links and add contact data

---
+++ src/lib/constants/contact.ts
@@ -0,0 +1,4 @@
+export const phone = '+52 449 340 2622'
+export const phoneClean = '+524493402622'
+export const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneClean.replace('+52', '521')}&text=Hola%2C%20te%20contacto%20desde%20tu%20web%20software3s.com`
+export const email = 'contact@software3s.com'
